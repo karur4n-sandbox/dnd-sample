f19bca123f0d576abcbf55bd5282174939e18e1e
karur4n <karuran.f@gmail.com>
2016-07-12 15:38:41 +0900

    Rubocop にしたがって修正する

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,7 @@
 require 'synchro'
 
 class PostController < ApplicationController
-  protect_from_forgery :except => [:create]
+  protect_from_forgery except: [:create]
 
   def new
   end
@@ -9,8 +9,9 @@ class PostController < ApplicationController
   def create
     upload_file = params[:csv]
     name = upload_file.original_filename
+
     if ['.csv'].include?(File.extname(name).downcase) && upload_file.content_type == 'text/csv'
-      File.open("csv/#{Time.now.to_i}.csv", 'wb') { |f|f.write(upload_file.read) }
+      File.open("csv/#{Time.now.to_i}.csv", 'wb') { |f| f.write(upload_file.read) }
       Synchro::Synchro.run
     else
       p 'invalid なファイルです。'
